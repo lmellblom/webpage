a8c9dfb0bf3254e41633f8115d483072246306b7
Linnéa Mellblom <lmellblom@users.noreply.github.com>
2015-08-18 11:23:34 +0200

    added new style to the webpage. clean

--- cv/cvInfo.js
+++ cv/cvInfo.js
@@ -2,7 +2,7 @@
 var app = angular.module('cvInfo', []);
 app.controller('cvCtrl', function($scope) {
 
-	$scope.imageOfMeSrc = "pics/linnea.jpg"; 
+	$scope.imageOfMeSrc = "../pics/linnea2.jpg"; 
 
 	$scope.me = {
 		header: "Vem är jag?",
